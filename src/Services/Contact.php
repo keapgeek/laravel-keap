e202e4ff6621edf4eb0f36a926946d3797615d4f
Azzarip <paride21azzari@gmail.com>
2024-04-21 10:15:03 +0200

    added create method

--- src/Services/Contact.php
+++ src/Services/Contact.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Azzarip\Keap\Services;
+use Azzarip\Keap\Exceptions\KeapException;
 
 class Contact extends Service
 {
@@ -14,5 +15,15 @@ class Contact extends Service
     public function get(int $id)
     {
         return $this->client->get($id);
+    }
+
+    public function create(array $data)
+    {
+        if (!array_key_exists('email_addresses', $data) && ! array_key_exists('phone_numbers', $data)) {
+            throw new KeapException('Missing Email addresses and/or phone numbers');
+        }
+
+        return $this->client->post('/', $data);
+
     }
 }

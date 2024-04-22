f499224c344ec1e8f226925c5a746cb66c3c0521
Azzarip <paride21azzari@gmail.com>
2024-04-22 07:52:25 +0200

    fixed contact goal

--- src/Services/Job.php
+++ src/Services/Job.php
@@ -3,11 +3,12 @@
 namespace Azzarip\Keap\Services;
 
 use Azzarip\Keap\Jobs\AchieveGoal;
+use Azzarip\Keap\Interfaces\KeapInterface;
 
 class Job
 {
-    public function contactGoal($contact, string $callName)
+    public function contactGoal(KeapInterface $contact, string $callName)
     {
-        return AchieveGoal::dispatchAfterResponse($contact->id, $callName);
+        return AchieveGoal::dispatchAfterResponse($contact, $callName);
     }
 }

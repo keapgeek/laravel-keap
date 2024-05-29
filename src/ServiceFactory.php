2659d8a4a01826425477445c6f1b814649dc0fce
Azzarip <paride21azzari@gmail.com>
2024-05-29 22:56:07 +0200

    added tag to service factory

--- src/ServiceFactory.php
+++ src/ServiceFactory.php
@@ -57,6 +57,10 @@ class ServiceFactory
     {
         return new Services\Oauth();
     }
+    public function tag()
+    {
+        return new Services\Tag();
+    }
     public function user()
     {
         return new Services\User();

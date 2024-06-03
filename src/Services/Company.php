1fc90ef5d577dbb21271654a25a847ab2745ba10
Azzarip <paride21azzari@gmail.com>
2024-06-03 08:39:38 +0200

    update has opt_in_reason

--- src/Services/Company.php
+++ src/Services/Company.php
@@ -36,7 +36,9 @@ class Company extends Service
         if (!array_key_exists('company_name', $data) || is_null($data['company_name'])) {
             throw new ValidationException('Required: company_name');
         }
-
+        if (!array_key_exists('opt_in_reason', $data) || is_null($data['opt_in_reason'])) {
+            $data['opt_in_reason'] = config('keap.opt_in_reason');
+        }
         return $this->client->patch("/$company_id", $data);
     }
 

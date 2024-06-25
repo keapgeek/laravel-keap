2b4d260e1fad013b2c553f1b06428c809cc782f5
Azzarip <paride21azzari@gmail.com>
2024-06-25 08:42:36 +0200

    added

--- src/Services/Form.php
+++ src/Services/Form.php
@@ -8,6 +8,8 @@ class Form
 {
     public function __construct(string $form_xid, array $data, array $headers = []){
         if(empty(config('keap.app_name'))) return;
+        $data['inf_form_xid'] = $form_xid;
+        //$data['inf_form_name'] = 'Web Form Submitted';
 
         Http::withHeaders($headers)->asForm()->post('https://' . config('keap.app_name'). '.infusionsoft.com/app/form/process/' . $form_xid, $data);
     }

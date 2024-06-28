59920770138d74794014bcbb5e73fb607db61714
Azzarip <paride21azzari@gmail.com>
2024-06-28 16:47:31 +0200

    parsing email fields

--- src/Services/Email.php
+++ src/Services/Email.php
@@ -2,12 +2,22 @@
 
 namespace KeapGeek\Keap\Services;
 
+use Illuminate\Support\Carbon;
+
 class Email extends Service
 {
     protected $uri = '/v1/emails';
 
     public function list(array $data = [])
     {
+        if (array_key_exists('since_sent_date', $data)) {
+            $data['since_sent_date'] = Carbon::parse($data['since_sent_date'])->setTimezone('UTC')->format('Y-m-d\TH:i:s.v\Z');
+        }
+
+        if (array_key_exists('until_sent_date', $data)) {
+            $data['until_sent_date'] = Carbon::parse($data['until_sent_date'])->setTimezone('UTC')->format('Y-m-d\TH:i:s.v\Z');
+        }
+
         $list = $this->client->get('/', $data);
 
         return $list['emails'];

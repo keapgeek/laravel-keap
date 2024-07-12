9952803c10ca7bd79a9dce9dc061724181f7aeaf
Azzarip <paride21azzari@gmail.com>
2024-07-12 11:11:56 +0200

    fixed style

--- resources/views/connected.blade.php
+++ resources/views/connected.blade.php
@@ -9,7 +9,7 @@
 <body>
     <div class="flex flex-col items-center justify-center min-h-screen gap-5 bg-gray-200">
         <div class="w-full max-w-2xl p-6 bg-white rounded-lg shadow-lg">
-            <h1 class="mb-4 text-2xl font-bold text-center">Successfull connection!</h1>
+            <h1 class="mb-4 text-2xl font-bold text-center">Keap Successfully Connected!</h1>
         <a href="/" class="block w-full max-w-lg py-2 mx-auto font-semibold text-center bg-green-400 border-4 border-green-600 rounded-lg hover:bg-green-600">Back to Homepage</a>
 
         </div>
@@ -17,12 +17,12 @@
             <h2 class="mb-4 text-2xl font-bold text-center">Laravel Keap Docs</h2>
             <p class="my-2 text-center">Learn more about Laravel Keap</p>
             <a href="https://www.laravelkeap.com?utm_source=lk-package&utm_medium=connected-view"
-            class="block w-full py-2 font-semibold text-center text-black bg-green-400 border-2 border-green-600 rounded-lg hover:bg-green-600">Visit the docs</a>
+            class="block w-full py-2 font-semibold text-center text-black bg-green-100 border-2 border-green-600 rounded-lg hover:bg-green-200">Visit the docs</a>
             <p class="mt-4">package brought to you by:</p>
             <h2 class="my-4 text-2xl font-bold text-center">Keap Geek</h2>
             <p class="my-2 text-center">Advanced Keap Training</p>
             <a href="https://www.keapgeek.com?utm_source=lk-package&utm_medium=connected-view"
-            class="block w-full py-2 font-semibold text-center text-black bg-green-400 border-2 border-green-600 rounded-lg hover:bg-green-600">Learn advanced Keap Automations</a>
+            class="block w-full py-2 font-semibold text-center text-black bg-blue-200 border-2 border-blue-600 rounded-lg hover:bg-blue-400">Learn advanced Keap Automations</a>
 
         </div>
     </div>

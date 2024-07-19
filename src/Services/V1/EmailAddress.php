96df82d9c2a314b9288f7a9e2f0d086211fd19a6
Azzarip <paride21azzari@gmail.com>
2024-07-19 18:48:02 +0200

    changed email address method to updateStatus

--- src/Services/V1/EmailAddress.php
+++ src/Services/V1/EmailAddress.php
@@ -8,7 +8,7 @@ class EmailAddress extends Service
 {
     protected $uri = '/v1/emailAddresses';
 
-    public function replace(string $email, bool $opted_in, ?string $opt_in_reason = null)
+    public function updateStatus(string $email, bool $opted_in, ?string $opt_in_reason = null)
     {
         $this->put("/$email", [
             'opted_in' => $opted_in,
--- tests/Feature/Services/V1/EmailAddressTest.php
+++ tests/Feature/Services/V1/EmailAddressTest.php
@@ -14,7 +14,7 @@ test('facade returns a EmailAddress Service', function () {
 
 test('replace makes a PUT request', function () {
     Http::fake();
-    Keap::emailAddress()->replace('email@example.com', true);
+    Keap::emailAddress()->updateStatus('email@example.com', true);
 
     Http::assertSent(function ($request) {
         return $request->url() === 'https://api.infusionsoft.com/crm/rest/v1/emailAddresses/email@example.com' &&
